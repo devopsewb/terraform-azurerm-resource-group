1201481ef2ee045e9bda0c0fc5c554f4c8aef743
devopsewb <53023609+devopsewb@users.noreply.github.com>
2019-12-11 16:11:38 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 resource "azurerm_resource_group" "rg" {
   name     = "${var.name}"
   location = "${var.location}"
-  tags     = "${var.tags}"
+  tags     =   tags = {
+    environment = "${var.environment}"
+    cost_center = "${var.cost_center }"
+  }
 }
+
